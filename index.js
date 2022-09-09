e4f2b04f19db63f510951777b6abccf647b9c6a7
filipe bernardo <filipebernardo.fb@gmail.com>
2022-09-09 17:04:04 +0100

    changed character speeds to 4

--- index.js
+++ index.js
@@ -192,10 +192,10 @@ function animate(){
 
 	//player movement
 	if (keys.a.pressed && player.lastKey === 'a') {
-		player.velocity.x = -5
+		player.velocity.x = -4
 		player.switchSprite('run')
 	} else if (keys.d.pressed && player.lastKey === 'd') {
-		player.velocity.x = 5
+		player.velocity.x = 4
 		player.switchSprite('run')
 	} else {
 		player.switchSprite('idle')
@@ -210,10 +210,10 @@ function animate(){
 
 	//enemy movement
 	if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-		enemy.velocity.x = -5
+		enemy.velocity.x = -4
 		enemy.switchSprite('run')
 	} else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-		enemy.velocity.x = 5
+		enemy.velocity.x = 4
 		enemy.switchSprite('run')
 	} else {
 		enemy.switchSprite('idle')
